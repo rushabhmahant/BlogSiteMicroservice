638b8099a0771c1f731eeef2c01023f63b9ad9db
unknown <rushabhmahant555@gmail.com>
2025-11-15 19:25:59 +0530

    Changes for cors policy issue fix

--- src/main/java/com/blogsite/security/SecurityConfig.java
+++ src/main/java/com/blogsite/security/SecurityConfig.java
@@ -1,11 +1,15 @@
 package com.blogsite.security;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 public class SecurityConfig {
@@ -27,6 +31,7 @@ public class SecurityConfig {
     }
     */
 	
+	/* Working method below
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.csrf().disable() 
@@ -34,5 +39,27 @@ public class SecurityConfig {
 
 		return http.build();
 	}
+	*/
+	
+	@Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+    	
+    	CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type", "Access-Control-Allow-Origin", "status"));
+        corsConfiguration.setAllowedOriginPatterns(List.of("*"));
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PUT","OPTIONS","PATCH", "DELETE"));
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setExposedHeaders(List.of("Authorization"));
+        
+    	
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+         
+        http.authorizeHttpRequests()
+                .anyRequest().permitAll()
+                .and().csrf().disable().cors().configurationSource(request -> corsConfiguration);
+         
+        return http.build();
+    }
 
 }
